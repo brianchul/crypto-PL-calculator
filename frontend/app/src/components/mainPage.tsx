3adc7704db8fb0ddf32eeac786e07261a058363f
brianchul <brianchul2000@gmail.com>
2021-07-01 01:12:32 +0800

    cleanup

--- frontend/app/src/components/mainPage.tsx
+++ frontend/app/src/components/mainPage.tsx
@@ -5,8 +5,7 @@ import TradingChart from './tradingChart'
 import { IAddressData } from '../models/address'
 import { Button, Col, Input, Row } from 'antd'
 
-const MainPage:FC=()=>{
-    const [account, setAccount] = useState("");
+const MainPage: FC = () => {
     const [url, setFullUrl] = useState("/account/");
     const baseUrl = "/account/"
     const { status, errMsg, data, clearUrl, fetchData } = useFetchOne<IAddressData[]>()
@@ -16,39 +15,39 @@ const MainPage:FC=()=>{
     const FetchAccount = async () => {
         await fetchData(url)
     }
-    const GetAccount = (acc:string) => {
-        setAccount(acc)
+    const GetAccount = (acc: string) => {
         setFullUrl(baseUrl + acc)
     }
 
     const ShowBackend = () => {
         setShowBackendBtn(!showBackendBtn)
-        if(!showBackendBtn)
+        if (!showBackendBtn)
             setChangeShowBackendBtn("hide backend api data")
         else
             setChangeShowBackendBtn("show backend api data")
     }
 
-   
+
     return (
         <Row>
-            <Col span={10}  offset={1}>
-                <Input placeholder="Address" onChange={(e) => {GetAccount(e.target.value)}} />
+            <Col span={10} offset={1}>
+                <Input placeholder="Address" onChange={(e) => { GetAccount(e.target.value) }} />
                 <Button onClick={FetchAccount}>
                     get account
                 </Button>
 
                 <Button onClick={clearUrl}>clear content</Button>
                 <Button onClick={ShowBackend}>{changeShowBackendBtn}</Button>
-                
+
                 <div>status: {status}</div>
                 <div>Error message: {errMsg}</div>
-                <div id="showApi">{showBackendBtn ? JSON.stringify(data) : ""}</div>
+                <div id="showApi">{showBackendBtn ?
+                    <Input.TextArea rows={4} value={JSON.stringify(data)} /> : ""}
+                </div>
                 <Table api={data.current} />
             </Col>
             <Col span={10} offset={1}>
-                <TradingChart/>
-
+                <TradingChart />
             </Col>
         </Row>
     )
